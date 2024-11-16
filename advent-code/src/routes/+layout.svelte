4940de03acaa0d8c1a249376341ec858ddd2ea9a
Rowiz49 <adrianpuyetm@gmail.com>
2024-11-16 11:51:48 +0100

    Add base layout and christmas tree

---
+++ advent-code/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { children } = $props();
+</script>
+
+<header>
+    <img src="/logo.png" alt="logo" class="logo">
+
+    <a href="/login" aria-label="Log In" class="session">Iniciar Sesión</a>
+</header>
+
+<link rel="stylesheet" href="/global.css" />
+
+{@render children()}
--- advent-code/src/routes/+page.svelte
+++ advent-code/src/routes/+page.svelte
@@ -1,3 +1,44 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="tree">
+    <pre>
+             /\
+            &lt;  &gt;
+             \/
+             /\
+            /  \
+           /++++\
+          /  ()  \
+          /      \
+         /~`~`~`~`\
+        /  ()  ()  \
+        /           \
+       /*&*&*&*&*&*&\
+      /  ()  ()  ()  \
+      /              \
+     /++++++++++++++++\
+    /  ()  ()  ()  ()  \
+    /                  \
+   /~`~`~`~`~`~`~`~`~`~`\
+  /  ()  ()  ()  ()  ()  \
+  /*&*&*&*&*&*&*&*&*&*&*&\
+ /                        \
+/,.,.,.,.,.,.,.,.,.,.,.,.,.\
+           |    |
+           <a href="/2024" class="problem-select">[2024]</a>
+        </pre>
+
+</div>
+
+<style>
+    *{
+        font-family: monospace;
+    }
+    .tree{
+        padding-top: 8px;
+        font-family: monospace;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+</style>
