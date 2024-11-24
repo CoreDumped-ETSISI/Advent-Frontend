1fd340958cb21e08d74a3cf4350d6f63d9178518
Rowiz49 <adrianpuyetm@gmail.com>
2024-11-24 11:44:48 +0100

    Add user_id to template, placeholder for username

--- advent-code/src/routes/+layout.server.ts
+++ advent-code/src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad,Actions } from './$types.ts';
  
 export const load = (async ({ cookies }) => {
   const token = cookies.get('AuthorizationToken');
- 
   return {
     token
   };
--- advent-code/src/routes/+layout.svelte
+++ advent-code/src/routes/+layout.svelte
@@ -2,12 +2,18 @@
 	import type { Snippet } from 'svelte';
 	import type { LayoutData, PageData } from './$types.ts';
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+	let userid = $state('');
+	$effect(() => {
+		if (data.token) {
+			userid = JSON.parse(atob(data.token.split('.')[1])).user_id;
+		}
+	});
 </script>
 
-<head>
+<svelte:head>
 	<title>Adviento Core Dumped</title>
-</head>
-<link rel="stylesheet" href="/global.css" />
+	<link rel="stylesheet" href="/global.css" />
+</svelte:head>
 
 <header>
 	<a href="/" class="void"><img src="/logo.png" alt="logo" class="logo" /></a>
@@ -15,7 +21,7 @@
 		<a href="/register" aria-label="Register" class="session">Register</a>
 		<a href="/login" aria-label="Log In" class="session">Iniciar Sesión</a>
 	{:else}
-		<a href="/logout" aria-label="Log Out" class="session">Cerrar Sesión</a>
+		<a href="/logout" aria-label="Log Out" class="session">Cerrar Sesión {userid}</a>
 	{/if}
 </header>
 
